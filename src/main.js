22e2d0dd19716667419c843b902df09d32bd4f0c
johnnyrayalt <johnnyrayalt@gmail.com>
2018-04-27 12:34:07 -0700

    moved api call and parsing to its own file

--- src/main.js
+++ src/main.js
@@ -2,24 +2,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { Doctor } from './apicall.js';
+import { parser } from './jsonparse.js'
 
 $(document).ready(function() {
   $('#doctorSearchForm').submit(function(event) {
     event.preventDefault();
-    let openCall = new Doctor();
-    let newRequest = openCall.apiCall();
-
-    newRequest.then(
-      function(response) {
-        let body = JSON.parse(response);
-        let results = body;
-        // console.log(results);
-        $('#doctorsView').html(`<p>${results}</p>`);
-      },
-      function(error) {
-        $('#errorHandling').html(`There was an error processing your request: ${error.message}`)
-      }
-    )
-  })
+    parser();
+    });
 });
