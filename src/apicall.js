d5a5fa88d692fdb40b178b7cd89d6855101c8938
johnnyrayalt <johnnyrayalt@gmail.com>
2018-04-27 11:39:47 -0700

    moved api key to .env file

--- src/apicall.js
+++ src/apicall.js
@@ -1,14 +1,14 @@
 class Doctor {
   apiCall() {
-    return new new Promise(function(resolve, reject) {
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=45.543%2C-122.794%2C100&user_location=45.543%2C-122.794&skip=0&limit=1&user_key=2d7e51a2fadacc4242b1c07238fd2680';
+    return new Promise(function(resolve, reject) {
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.543%2C-122.794%2C100&user_location=45.543%2C-122.794&skip=0&limit=1&user_key=${process.env.exports.apiKey}`;
       let xhr = new XMLHttpRequest();
 
       xhr.onload = function() {
         if (this.status === 200) {
           resolve(xhr.response);
         } else {
-          reject(Error(error.statusText));
+          reject(Error(xhr.statusText));
         }
       }
 
